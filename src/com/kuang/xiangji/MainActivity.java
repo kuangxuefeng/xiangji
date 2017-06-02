68f50bd6b9a12d87923a5cdc3725292e248f37c7
kuangxuefeng <1024883177@qq.com>
2017-06-02 23:22:17 +0800

    去掉了多余的代码

--- src/com/kuang/xiangji/MainActivity.java
+++ src/com/kuang/xiangji/MainActivity.java
@@ -1,6 +1,5 @@
 package com.kuang.xiangji;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
@@ -17,12 +16,12 @@ import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
-import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
 import android.hardware.Camera.AutoFocusCallback;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.View;
@@ -36,9 +35,14 @@ import com.kuang.xiangji.MySurfaceView.MyCallBack;
 
 public class MainActivity extends Activity implements OnClickListener,
 		AutoFocusCallback {
+	private static final String TAG = "MainActivity";
 
 	SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 	SimpleDateFormat sdfFile = new SimpleDateFormat("yyyyMMddHHmmss");
+	
+	//(176*144,320*240,352*288,480*360,640*480) 
+	private int imageW = 640;
+	private int imageH = 480;
 
 	MySurfaceView mySurface;// surfaceView声明
 	SurfaceHolder holder;// surfaceHolder声明
@@ -49,47 +53,25 @@ public class MainActivity extends Activity implements OnClickListener,
 
 	/**
 	 * 预览的参数：myParameters.setPreviewSize(1280, 720）
-
-图片参数：myParameters.setPictureSize(2048, 1152); //1280, 720
-
-图片最终尺寸:宽600 * 高800
+	 * 
+	 * 图片参数：myParameters.setPictureSize(2048, 1152); //1280, 720
+	 * 
+	 * 图片最终尺寸:宽600 * 高800
 	 */
 	// 创建jpeg图片回调数据对象
 	PictureCallback jpeg = new PictureCallback() {
+
 		@Override
 		public void onPictureTaken(byte[] data, Camera camera) {
 			// TODO Auto-generated method stub
 			try {// 获得图片
 				Bitmap bm = BitmapFactory.decodeByteArray(data, 0, data.length);
-				
-//				String SavePath = getSDCardPath() + "/AndyDemo/ScreenImage/a";
-//				File path = new File(SavePath);
-//				// 文件
-//				String filepath = SavePath + "/Screen22_"
-//						+ sdfFile.format(new Date()) + ".png";
-//				File file = new File(filepath);
-//				if (!path.exists()) {
-//					path.mkdirs();
-//				}
-//				if (!file.exists()) {
-//					file.createNewFile();
-//				}
-//				
-//	            BufferedOutputStream bos =      
-//	                new BufferedOutputStream(new FileOutputStream(file));      
-//	            bm.compress(Bitmap.CompressFormat.PNG, 100, bos);//将图片压缩到流中      
-//	            bos.flush();//输出      
-//	            bos.close();//关闭 
-	            
-	            try {
-//					Bitmap bitmap = BitmapFactory.decodeFile(filepath);
-					saveImage(bm);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-	            
-	            myCamera.startPreview();// 开启预览
+				Log.e(TAG, "bm="+bm);
+				myCamera.startPreview();// 开启预览
 				isClicked = false;
+
+				saveImage(bm);
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -100,8 +82,11 @@ public class MainActivity extends Activity implements OnClickListener,
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().setFlags(
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+						| WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+						| WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
@@ -148,7 +133,6 @@ public class MainActivity extends Activity implements OnClickListener,
 
 	@Override
 	public void onClick(View paramView) {
-		// GetandSaveCurrentImage();
 		if (!isClicked) {
 			myCamera.autoFocus(this);// 自动对焦
 			isClicked = true;
@@ -159,18 +143,20 @@ public class MainActivity extends Activity implements OnClickListener,
 	}
 
 	private void saveImage(Bitmap bmp) {
-		Bitmap Bmp = Bitmap.createBitmap(1280, 720, Config.ARGB_8888);
+		Bitmap Bmp = Bitmap.createBitmap(imageW, imageH, Config.ARGB_8888);
 		Bmp = bmp.copy(Config.ARGB_8888, true);
-		
+
 		Paint paint = new Paint();
+		Log.e(TAG, "Bmp.getHeight()=" + Bmp.getHeight());
 		paint.setTextSize(100);
 		paint.setColor(Color.RED);
 		paint.setTextAlign(Paint.Align.CENTER);
 		paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
 		Canvas canvas = new Canvas(Bmp);
-		canvas.drawText(getTimeString(), Bmp.getWidth()/2, Bmp.getHeight()/2, paint);
+		canvas.drawText(getTimeString(), Bmp.getWidth() / 2,
+				Bmp.getHeight() / 2, paint);
 
-		String SavePath = getSDCardPath() + "/AndyDemo/ScreenImage";
+		String SavePath = getBasePath();
 
 		// 3.保存Bitmap
 		try {
@@ -193,7 +179,7 @@ public class MainActivity extends Activity implements OnClickListener,
 				fos.flush();
 				fos.close();
 
-				Toast.makeText(this, "截屏文件已保存至SDCard/AndyDemo/ScreenImage/下",
+				Toast.makeText(this, "截屏文件已保存至"+getBasePath()+"下",
 						Toast.LENGTH_LONG).show();
 			}
 
@@ -202,60 +188,9 @@ public class MainActivity extends Activity implements OnClickListener,
 		}
 	}
 
-	/**
-	 * 获取和保存当前屏幕的截图
-	 */
-	private void GetandSaveCurrentImage() {
-		// 1.构建Bitmap
-		WindowManager windowManager = getWindowManager();
-		Display display = windowManager.getDefaultDisplay();
-		int w = display.getWidth();
-		int h = display.getHeight();
-
-		Bitmap Bmp = Bitmap.createBitmap(w, h, Config.ARGB_8888);
-
-		// 2.获取屏幕
-		View decorview = this.getWindow().getDecorView();
-		decorview.setDrawingCacheEnabled(true);
-		Bmp = decorview.getDrawingCache();
-
-		Paint paint = new Paint();
-		paint.setTextSize(5);
-		paint.setColor(Color.RED);
-		paint.setTextAlign(Paint.Align.CENTER);
-		paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
-		Canvas canvas = new Canvas(Bmp);
-		canvas.drawText(getTimeString(), 20, 20, paint);
-
-		String SavePath = getSDCardPath() + "/AndyDemo/ScreenImage";
-
-		// 3.保存Bitmap
-		try {
-			File path = new File(SavePath);
-			// 文件
-			String filepath = SavePath + "/Screen_1.png";
-			File file = new File(filepath);
-			if (!path.exists()) {
-				path.mkdirs();
-			}
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-
-			FileOutputStream fos = null;
-			fos = new FileOutputStream(file);
-			if (null != fos) {
-				Bmp.compress(Bitmap.CompressFormat.PNG, 90, fos);
-				fos.flush();
-				fos.close();
-
-				Toast.makeText(this, "截屏文件已保存至SDCard/AndyDemo/ScreenImage/下",
-						Toast.LENGTH_LONG).show();
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	private String getBasePath(){
+		String savePath = getSDCardPath() + "/feng/ScreenImage";
+		return savePath;
 	}
 
 	/**
@@ -280,12 +215,12 @@ public class MainActivity extends Activity implements OnClickListener,
 			// 设置参数,并拍照
 			Camera.Parameters params = myCamera.getParameters();
 			params.setPictureFormat(PixelFormat.JPEG);
-			params.setPreviewSize(1280, 720);
+			params.setPreviewSize(imageW, imageH);
 			myCamera.setParameters(params);
 			myCamera.takePicture(null, null, jpeg);
 		}
 	}
-	
+
 	private String getTimeString() {
 		return "测试(作者：风)： " + sdf.format(new Date());
 	}
